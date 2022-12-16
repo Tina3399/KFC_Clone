ab9dbc34cecd212677082b7e878a4a1a9fa6c6c1
Tina Rawat <tinarawat03@gmail.com>
2022-12-17 00:19:10 +0530

    Day-4 login/signup done

--- scripts/loginPage.js
+++ scripts/loginPage.js
@@ -1,5 +1,6 @@
 const form = document.querySelector("form");
-
+const wrong_email = document.getElementById("wrong_email");
+const wrong_password = document.getElementById("wrong_password");
 let userArray;
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -8,17 +9,25 @@ form.addEventListener("submit", (e) => {
     email: form.email.value,
     password: form.password.value,
   };
-  fetch_user_data();
+  fetch_user_data(login_data);
 });
 
-async function fetch_user_data() {
+async function fetch_user_data(login_data) {
   try {
     let response = await fetch(
-      "https://63986b8e044fa481d69ccef0.mockapi.io/users_data"
+      `https://63986b8e044fa481d69ccef0.mockapi.io/users_data?email=${login_data.email}`
     );
     let res = await response.json();
-    console.log(res);
+    if (res[0].password === login_data.password) {
+      res[0].isAuth = true;
+      localStorage.setItem("current_user", JSON.stringify(res));
+      window.location.href = "homePage.html";
+      console.log(res);
+    } else {
+      wrong_password.innerText = "Incorrect Password!";
+    }
   } catch (error) {
+    wrong_email.innerText = "Please enter correct Email!";
     console.log(error);
   }
 }
--- scripts/productPage.js
+++ scripts/productPage.js
@@ -1,13 +1,35 @@
-const product = JSON.parse(localStorage.getItem("product"));
+let product = JSON.parse(localStorage.getItem("product"));
+const current_user = JSON.parse(localStorage.getItem("current_user")) || [];
+
+if (current_user.length > 0) {
+  if (current_user[0].isAuth) {
+    user_name_div.style.display = "block";
+    user_name.innerText = current_user[0].name;
+    logout_icon.style.display = "block";
+    login_icon.style.display = "none";
+  }
+}
+const cart_arr = JSON.parse(localStorage.getItem("cart_arr")) || [];
 const increase_button = document.getElementById("increase");
+increase_button.addEventListener("click", increase_qty);
 const decrease_button = document.getElementById("decrease");
+decrease_button.addEventListener("click", decrease_qty);
 const total_quantity = document.getElementById("total_quantity");
+total_quantity.innerText = product.count;
+const user_login_tag = document.getElementById("user_login_tag");
+const add_to_cart_button = document.getElementById("add_to_cart_button");
+
+add_to_cart_button.addEventListener("click", add_to_cart);
 const container = document.getElementById("main_product");
 const name_tag = document.getElementById("product_name");
+const price_tag = document.getElementById("product_price");
 showData(product);
 
 function showData(product) {
+  console.log(product);
+
   name_tag.innerText = product.name;
+  price_tag.innerText = "₹" + product.price;
   let sub_div = document.createElement("div");
   let image_div = document.createElement("div");
   image_div.setAttribute("id", "product_image_div");
@@ -16,4 +38,57 @@ function showData(product) {
   image_div.append(image);
   sub_div.append(image_div);
   container.append(sub_div);
+}
+
+// function priceTotal() {
+//   let totalVal = 0;
+//   totalVal += +product.count * +product.price;
+//   price_tag.innerText = "₹" + totalVal;
+//   localStorage.setItem("product", JSON.stringify(product));
+//   console.log(totalVal);
+// }
+
+function increase_qty() {
+  +product.count++;
+  total_quantity.innerText = product.count;
+  // priceTotal();
+}
+
+function decrease_qty() {
+  if (total_quantity.innerText == 1) {
+    return;
+  }
+  +product.count--;
+  total_quantity.innerText = product.count;
+  // priceTotal();
+}
+
+function add_to_cart() {
+  if (current_user.length === 0) {
+    user_login_tag.style.display = "block";
+  } else {
+    let isPresent = false;
+    for (let i = 0; i < cart_arr.length; i++) {
+      if (cart_arr[i].id === product.id) {
+        isPresent = true;
+      }
+    }
+    if (!isPresent) {
+      cart_arr.push(product);
+    } else {
+      return;
+    }
+  }
+
+  localStorage.setItem("cart_arr", JSON.stringify(cart_arr));
+  console.log(current_user);
+  console.log(product);
+  console.log(cart_arr);
+}
+
+function logout_user() {
+  current_user[0].isAuth = false;
+
+  localStorage.clear("current_user");
+  location.reload();
 }
